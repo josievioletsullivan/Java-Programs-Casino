18252a00b82388d8be38d5d6da851c5958a1ffd5
NeevZeroni <wkbarrett20@gmail.com>
2024-01-14 01:33:44 -0600

    Going to bed. The screen pulls up something now. Probably will remove the ChoiceBox class when I get the chance. See you at Lunds Tomorrow

--- Card.java
+++ Card.java
@@ -1,8 +1,8 @@
 
 
 public class Card {
-    String suits[] = {"Hearts","Diamonds","Clubs","Spades"}; //these are clubs, hearts, spades, and diamonds in their purest forms
-    String faces[] = {"1","2","3","4","5","6","7","8","9","Jack","Queen","King","Ace"}; //these are the card faces in their purest forms
+    String suits[] = {"Hearts","Diamonds","Clubs","Spades"}; 
+    String faces[] = {"1","2","3","4","5","6","7","8","9","Jack","Queen","King","Ace"}; 
     int values[] = {1,2,3,4,5,6,7,8,9,10,10,10,10};
     int suit;
     int face;
--- Screen.java
+++ Screen.java
@@ -1,6 +1,6 @@
 //import some GUI repository
 import java.awt.FlowLayout;
-
+import java.awt.event.*;
 import javax.swing.*;
 public class Screen{
 	static int screenHeight;
@@ -9,22 +9,26 @@ public class Screen{
 	JLabel boxJLabels[] = new JLabel[boxCount];
 	String[] boxLabels = {"Hit","Stand","Double Down","Surrender"}; 
 	ChoiceBox[] Boxes = new ChoiceBox[boxCount];
+	JButton[] Buttons = new JButton[boxCount];
 	public Screen(){
-	    screenHeight = 500; //default values. Will probably change to a different value later
-	    screenWidth = 500;
+	    screenHeight = 1080; //default values. Will probably change to a different value later
+	    screenWidth = 1920;
 	    Mouse Mouse = new Mouse(screenHeight);
 		JFrame aFrame = new JFrame("Blackjack");
         aFrame.setSize(Screen.screenWidth,Screen.screenHeight);
         aFrame.setVisible(true);
 		aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		aFrame.setLayout(new FlowLayout());
+		gameStartScreen(aFrame);
 		createBoxes(aFrame);
 	}
 	void createBoxes(JFrame aFrame){
 	    for(int i = 0; i < boxCount; i++){
-	        Boxes[i] = new ChoiceBox(i,screenWidth, screenHeight,4);
+	        Boxes[i] = new ChoiceBox(i,screenWidth, screenHeight,4); //good chance says I'll get rid of choice boxes. I thought I'd have to draw these myself. Thank God.
+			Buttons[i] = new JButton(boxLabels[i]);
 			boxJLabels[i] = new JLabel(boxLabels[i]);
-			aFrame.add(boxJLabels[i],Boxes[i].leftBound);
+			aFrame.add(boxJLabels[i]);
+			aFrame.add(Buttons[i]);
 	    }
 	}
 	int checkClick(int x, int y){
@@ -34,6 +38,16 @@ public class Screen{
 	        }
 	    }
 	    return 0; //triggers the default case
+	}
+	void redraw(){
+		//clears the screen
+		//redraws the screen every time an event happens
+	}
+	void gameStartScreen(JFrame aFrame){
+		JLabel wager = new JLabel("How much do you want to wager?");
+		JTextField wagField = new JTextField(12);
+		aFrame.add(wager);
+		aFrame.add(wagField);
 	}
 }
 
