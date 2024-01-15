531e7b060ee99005aaff7cedf8e4adb4561cb613
NeevZeroni <wkbarrett20@gmail.com>
2024-01-14 21:38:02 -0600

    Restarting my computer rq committing everything I've changed. The screen is Green now

--- Screen.java
+++ Screen.java
@@ -1,47 +1,50 @@
 //import some GUI repository
-import java.awt.FlowLayout;
-import java.awt.event.*;
+import java.awt.*;
 import javax.swing.*;
-public abstract class Screen{
+public class Screen extends JFrame{
 	static int screenHeight;
 	static int screenWidth;
 	int boxCount = 4;
-	JLabel boxJLabels[] = new JLabel[boxCount];
+	//JLabel boxJLabels[] = new JLabel[boxCount];
 	String[] boxLabels = {"Hit","Stand","Double Down","Surrender"}; 
 	//ChoiceBox[] Boxes = new ChoiceBox[boxCount];
 	JButton[] Buttons = new JButton[boxCount];
+	JPanel pnl = new JPanel();
+	Color colorBackground = new Color(39,119,20);
 	public Screen(){
-	    screenHeight = 1080; //default values. Will probably change to a different value later
-	    screenWidth = 1920;
-		JFrame aFrame = new JFrame("Blackjack");
-        aFrame.setSize(Screen.screenWidth,Screen.screenHeight);
-        aFrame.setVisible(true);
-		aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		aFrame.setLayout(new FlowLayout());
-		gameStartScreen(aFrame);
-		createBoxes(aFrame);
+	    screenHeight = 500; //default values. Will probably change to a different value later
+	    screenWidth = 1000;
+        setSize(Screen.screenWidth,Screen.screenHeight);
+        setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		Board board = new Board();
+		setContentPane(board);
+		gameStartScreen();
+		createBoxes();
 	}
-	void createBoxes(JFrame aFrame){
+	void createBoxes(){
 	    for(int i = 0; i < boxCount; i++){
 	        //Boxes[i] = new ChoiceBox(i,screenWidth, screenHeight,4); //good chance says I'll get rid of choice boxes. I thought I'd have to draw these myself. Thank God.
 			Buttons[i] = new JButton(boxLabels[i]);
+			//Buttons[i].setLocation();
 			//boxJLabels[i] = new JLabel(boxLabels[i]);
-			//aFrame.add(boxJLabels[i]);
-			aFrame.add(Buttons[i]);
+			//add(boxJLabels[i]);
+			add(Buttons[i],0,i*screenHeight/boxCount);
 	    }
 	}
-	void redraw(JFrame aFrame){
-		aFrame.removeAll();
-		aFrame.revalidate();
-		aFrame.repaint();
+	void redraw(){
+		removeAll();
+		revalidate();
+		repaint();
 		//clears the screen
 		//redraws the screen every time an event happens
 	}
-	void gameStartScreen(JFrame aFrame){
+	void gameStartScreen(){
 		JLabel wager = new JLabel("How much do you want to wager?");
 		JTextField wagField = new JTextField(12);
-		aFrame.add(wager);
-		aFrame.add(wagField);
+		add(wager);
+		add(wagField);
 	}
 	void gamePlayScreen(realPlayer P, house H){
 		for(int i = 0; i < P.hand.size(); i++){
@@ -49,6 +52,12 @@ public abstract class Screen{
 		}
 		for(int i = 0; i < H.hand.size(); i++){
 			//create card at high position
+		}
+	}
+	public class Board extends JPanel{ //I'm following Jon's youtube tutorial (episode 2) on how to do this
+		public void paintComponent(Graphics g){
+			g.setColor(colorBackground);
+			g.fillRect(0,0,screenWidth,screenHeight);
 		}
 	}
 }
